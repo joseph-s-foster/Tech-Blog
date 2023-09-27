110049b2679e0163153b605faad58ed968fc0072
D3C0D1NG <joseph.s.foster@icloud.com>
2023-09-26 19:33:00 -0500

    added content to files

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [User],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { posts, loggedIn: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router
