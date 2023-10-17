d32c92e735c0a8ad0bc2cb00917f8395a8cedb9b
D3C0D1NG <joseph.s.foster@icloud.com>
2023-10-17 18:38:30 -0500

    adding files and updating content

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const { Post, Comment, User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Get all posts and JOIN with user data
     const postData = await Post.findAll({
@@ -13,10 +13,42 @@ router.get('/', async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { posts, loggedIn: req.session.logged_in });
+    res.render("homepage", { posts, loggedIn: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-module.exports = router
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const postData = await Post.findAll({
+      where: { user_id: req.session.user_id },
+    });
+
+    const posts = postData.map((post) =>
+      post.get({
+        plain: true,
+      })
+    );
+
+    res.render("dashboard", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ defaultLayout: "main", extname: '.hbs', helpers: helpers });
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -29,7 +29,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
